617c10cd24d628c2db077f9b441f469e4d15bd20
gtrujilloca <gtrujilloca@gmail.com>
2024-09-21 19:50:12 -0500

    feat: add initial config without ssr

---
+++ src/app/core/constants/links.ts
@@ -0,0 +1,8 @@
+import { ILink } from "../models/Ilink";
+
+export const links: ILink[] = [
+  { name: 'Home', path: '/' },
+  { name: 'About', path: '/about' },
+  { name: 'Contact', path: '/contact' },
+  { name: 'Pricing', path: '/pricing' },
+]
