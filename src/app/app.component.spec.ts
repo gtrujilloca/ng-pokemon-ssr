3e70c2cc94d2cc7bf41d5253a65f87f36b9a1155
gtrujilloca <gtrujilloca@gmail.com>
2024-10-08 22:07:42 -0500

    feat: add initial testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,52 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter, RouterOutlet } from '@angular/router';
+import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  template: '<span>Navbar</span>'
+})
+class NavbarComponentMock {}
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
+      imports: [
+        AppComponent,
+      ],
+      providers: [
+        provideRouter([])
+      ],
+    })
+    .overrideComponent(AppComponent,{
+      add: {
+        imports: [NavbarComponentMock]
+      },
+      remove: {
+        imports: [NavbarComponent]
+      }
+    })
+    .compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+  test('should create the app', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'pokemon-ssr' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokemon-ssr');
+  test('should have a router-outlet and app-navbar', () => {
+    const compiled = fixture.debugElement;
+
+    expect(compiled.query(By.css('router-outlet'))).toBeTruthy();
+    expect(compiled.query(By.css('app-navbar'))).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pokemon-ssr');
-  });
 });
